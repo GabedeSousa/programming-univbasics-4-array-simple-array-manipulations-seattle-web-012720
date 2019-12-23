5334c3ed6ab96be5f71840eaf4299ce531e0c687
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-23 22:12:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,10 +33,9 @@ end
 def using_concat(my_favorite_things,all_my_favs)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
-    @all_my_favs = my_favorite_things.concat(@my_favorite_things, more_favs)
+    @all_my_favs = @my_favorite_things.concat(more_favs)
   end
-
-
+  
 
 
 
