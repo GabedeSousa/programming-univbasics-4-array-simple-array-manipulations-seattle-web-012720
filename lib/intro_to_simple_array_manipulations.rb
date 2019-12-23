f9a93cecc742252e05de8696ccdf59d6ab19d069
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-23 21:53:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,15 +1,15 @@
-def using_push(array,string)
+def using_push(colors_in_the_rainbow)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   next_color = ("violet")
 colors_in_the_rainbow.push(next_color)
 end 
 
-def using_unshift(array,string)
+def using_unshift(bouroughs_in_nyc)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
 end
 
-def using_pop(array)
+def using_pop(continents)
 @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]  
 @continents.pop
 end
@@ -19,8 +19,13 @@ def pop_with_args(dog_breeds)
   @dog_breeds.pop(2)
 end
 
-def using_shift 
-  
+def using_shift(my_favorite_cities)
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @my_favorite_cities.shift 
+end
+
+
+
 
 
 
