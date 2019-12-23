2f3529df57afd1817f6b52158a45be4b838e8a3f
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-23 21:39:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,8 +10,8 @@ def using_unshift(array,string)
 end
 
 def using_pop(array)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]  
-continents.pop
+@continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]  
+@continents.pop
 end
 
 
