95354c8a7f6f883b155e5344a6c259adfbfc7d0b
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-23 21:59:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,9 @@ end
 def using_unshift(bouroughs_in_nyc)
   @bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   @new_neighborhood = "Staten Island"
-  
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
 
 def using_pop(continents)
 @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]  
