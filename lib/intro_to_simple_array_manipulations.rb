ac24c2a024de18c90e13ed7cbcb6f42124e7bbee
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-23 21:50:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,10 +14,11 @@ def using_pop(array)
 @continents.pop
 end
 
-def pop_with_args(array)
+def pop_with_args(dog_breeds)
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   @dog_breeds.pop(2)
 end
+
 
 
 
